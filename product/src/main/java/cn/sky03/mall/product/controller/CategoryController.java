36bba9974ec190f64e7096d069410fe56dc4e3da
ListenV <i@sky03.cn>
2022-11-25 21:16:20 +0800

    全部加上配置中心和注册中心

--- product/src/main/java/cn/sky03/mall/product/controller/CategoryController.java
+++ product/src/main/java/cn/sky03/mall/product/controller/CategoryController.java
@@ -1,21 +1,16 @@
 package cn.sky03.mall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
+import cn.sky03.common.utils.R;
+import cn.sky03.mall.product.entity.CategoryEntity;
+import cn.sky03.mall.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import cn.sky03.mall.product.entity.CategoryEntity;
-import cn.sky03.mall.product.service.CategoryService;
-import cn.sky03.common.utils.PageUtils;
-import cn.sky03.common.utils.R;
-
-
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 商品三级分类
@@ -31,16 +26,13 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装起来
      */
-    @RequestMapping("/list")
-    //@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("data", entities);
     }
-
 
     /**
      * 信息
--- product/src/main/java/cn/sky03/mall/product/entity/CategoryEntity.java
+++ product/src/main/java/cn/sky03/mall/product/entity/CategoryEntity.java
@@ -1,10 +1,12 @@
 package cn.sky03.mall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * 商品三级分类
@@ -56,4 +58,6 @@ public class CategoryEntity implements Serializable {
 	 */
 	private Integer productCount;
 
+	@TableField(exist = false)
+	private List<CategoryEntity> children;
 }
